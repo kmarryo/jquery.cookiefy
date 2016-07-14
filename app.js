a90eb3cbd633e208784e2cbbc02b2724ef0bfca7
Mario Lemke <m.lemke@ellusion.de>
2016-07-14 17:29:39 +0200

    created gh-pages

---
+++ app.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('body').cookiefy({
+        devMode: true,
+        backgroundColor: '#ffffcc',
+        color:'#404040',
+        fontFamily: 'Trebuchet MS',
+        fontSize: '13px',
+        borderTop: '1px solid #404040'
+    });
+});
