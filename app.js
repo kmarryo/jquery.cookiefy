d2e585660a3b00c0a38cc4e7040970ac8642a5e1
Mario Lemke <m.lemke@ellusion.de>
2016-07-20 09:32:44 +0200

    New app.js with new params

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 $(function () {
     $('body').cookiefy({
         displayedHtml: 'We use cookies to ensure that we give you the best experience on our website. If you continue, you agree with our <a href="#"> cookie policy</a>.',
-        backgroundColor: '#5bc0de',
-        color:'#fff',
+        backgroundColor: '#ffffcc',
+        color:'#404040',
         fontFamily: 'Trebuchet MS',
-        fontSize: '20px',
+        fontSize: '13px',
         borderTop: '1px solid #404040',
-        devMode: true
+        devMode: true,
+        closeButtonUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAMAAAAM7l6QAAAABlBMVEX///////9VfPVsAAAAAXRSTlMAQObYZgAAAE9JREFUeAHt08EJRCEMBuGZ/pveo0LMsAU8b/4fiEkIoLyPgqqLKm7uxb5VFj/p7UOHj2R43/u1/ktX0n04Gg7lUN6qrdqqm37z9e8tgcV/jw0BcORWlJMAAAAASUVORK5CYII='
     });
 });
