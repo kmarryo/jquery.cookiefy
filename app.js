0f2cc9ce40351b38cd665a49471ff7f497512af9
Mario Lemke <m.lemke@ellusion.de>
2016-07-20 09:37:19 +0200

    Color changes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 $(function () {
     $('body').cookiefy({
         displayedHtml: 'We use cookies to ensure that we give you the best experience on our website. If you continue, you agree with our <a href="#"> cookie policy</a>.',
-        backgroundColor: '#ffffcc',
-        color:'#404040',
+        backgroundColor: '#5bc0de',
+        color:'#fff',
         fontFamily: 'Trebuchet MS',
-        fontSize: '13px',
+        fontSize: '20px',
         borderTop: '1px solid #404040',
         devMode: true,
         closeButtonUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAMAAAAM7l6QAAAABlBMVEX///////9VfPVsAAAAAXRSTlMAQObYZgAAAE9JREFUeAHt08EJRCEMBuGZ/pveo0LMsAU8b/4fiEkIoLyPgqqLKm7uxb5VFj/p7UOHj2R43/u1/ktX0n04Gg7lUN6qrdqqm37z9e8tgcV/jw0BcORWlJMAAAAASUVORK5CYII='
